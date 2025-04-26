e13b529d08171af8423d691c42436dd88197b28b
LauraVanesa06 <168228452+LauraVanesa06@users.noreply.github.com>
2025-04-25 20:38:53 -0500

    ...

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,7 +6,19 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get "home" => "home#index", as: :index
+
+  devise_scope :user do
+    authenticated :user do
+      root to: "home#index", as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root to: "devise/sessions#new", as: :unauthenticated_root
+    end
+  end
+    # Otras rutas que tengas
+  
+  
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
