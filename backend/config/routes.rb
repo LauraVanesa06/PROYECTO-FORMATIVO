2cf87ef13292c82d48505c3220d8c3db763dacf8
LauraVanesa06 <168228452+LauraVanesa06@users.noreply.github.com>
2025-04-27 15:57:09 -0500

    Feature/33 (#41)
    * ...
    
    * ...

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -7,6 +7,19 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  devise_scope :user do
+    authenticated :user do
+      root to: "home#index", as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root to: "devise/sessions#new", as: :unauthenticated_root
+    end
+  end
+    # Otras rutas que tengas
+  
+  
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
