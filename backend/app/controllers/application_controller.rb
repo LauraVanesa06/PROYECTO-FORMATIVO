07584cd271cd692bc6c3d518b8367aeb53b97e29
KeinerL <168228409+KeinerL@users.noreply.github.com>
2025-05-05 15:54:24 -0500

    dashboard fusionado (#92)
    * cerrar sesion
    
    * ready

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  def after_sign_in_path_for(resource)
+    # Aquí defines la ruta a la que deseas redirigir
+    # Puede ser una ruta nombrada como:
+    dashboard_path # Por ejemplo, redirige a /productos
+  end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "dashboard" => "dashboard#index"
+  get "inventario" => "dashboard#inventario"
+  get "ventas" => "dashboard#ventas"
+  get "proveedores" => "dashboard#proveedores"
+  get "clientes" => "dashboard#clientes"
+
+
   root "home#index", as: :authenticated_root
   get 'productos', to: 'productos#productos'
   get 'contactos', to: 'contactos#contacto'
