414ce1369db468da6e741f7066290ebd2823d0c2
KeinerL <168228409+KeinerL@users.noreply.github.com>
2025-05-13 11:37:57 -0500

    roles done (#108)

--- backend/app/controllers/dashboard_controller.rb
+++ backend/app/controllers/dashboard_controller.rb
@@ -2,4 +2,11 @@ class DashboardController < ApplicationController
   def index
   end
   layout false
+
+  before_action :authenticate_user!
+  before_action :only_admins
+
+  def only_admins
+    redirect_to authenticated_root_path, alert: "No autorizado" unless current_user&.admin?
+  end
 end
