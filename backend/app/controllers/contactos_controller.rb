71a19bb381136c2f727036230c9d80229438a1b4
majo <168229992+MajoSanchezA@users.noreply.github.com>
2025-05-16 14:55:53 -0500

    Feature/91 (#109)
    * fix error estilos
    
    * Update application.css
    
    * update

--- backend/app/controllers/contactos_controller.rb
+++
@@ -1,5 +0,0 @@
-class ContactosController < ApplicationController
-  skip_before_action :authenticate_user!, onli: [:contactos]
-  def contacto
-  end
-end
--- backend/test/controllers/home_controller_test.rb
+++ backend/test/controllers/home_controller_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get home_index_url
+    assert_response :success
+  end
+
+  test "should get home" do
+    get home_home_url
+    assert_response :success
+  end
+
+  test "should get producto" do
+    get home_producto_url
+    assert_response :success
+  end
+
+  test "should get contacto" do
+    get home_contacto_url
+    assert_response :success
+  end
 end
