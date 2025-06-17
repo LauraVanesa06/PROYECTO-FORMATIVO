2f419b66b2d6b9a210e97d10d395eb4045954af9
KylieAndrea <168228628+KylieAndrea@users.noreply.github.com>
2025-06-16 20:33:57 -0500

    termine :)

--- backend/app/models/product.rb
+++ backend/app/models/product.rb
@@ -4,5 +4,5 @@ class Product < ApplicationRecord
   belongs_to :supplier
   has_many :purchasedetail, dependent: :destroy
 
-  has_one_attached :imagen, dependent: :purge_later
+  has_one_attached :imagen, dependent: :purge
 end
