adb997ccd532e7e024739ff338ac540968d0692d
KylieAndrea <168228628+KylieAndrea@users.noreply.github.com>
2025-06-26 01:29:48 -0500

    termine

--- backend/app/controllers/dashboard_controller.rb
+++ backend/app/controllers/dashboard_controller.rb
@@ -89,31 +89,56 @@ class DashboardController < ApplicationController
     @customers = Customer.all
     @customer = nil
     @purchasedetails = []
+    @buys = Buy.all
 
     if params[:documento].present?
       doc = params[:documento].to_i
       @customers = @customers.where(documento: doc)
     end
 
-    if params[:name].present?
-      @customers = @customers.where("nombre LIKE ?", "%#{params[:name]}%")
+    if params[:year].present? || params[:month].present? || params[:day].present?
+      conditions = []
+      values = []
+
+      if params[:year].present?
+        conditions << "strftime('%Y', buys.fecha) = ?"
+        values << params[:year]
+      end
+
+      if params[:month].present?
+        conditions << "strftime('%m', buys.fecha) = ?"
+        values << params[:month].rjust(2, '0')
+      end
+
+      if params[:day].present?
+        conditions << "strftime('%d', buys.fecha) = ?"
+        values << params[:day].rjust(2, '0')
+      end
+
+      @buys = @buys.where(conditions.join(" AND "), *values)
     end
+
+    buy_ids = @buys.pluck(:id)
 
     @customers = @customers.left_outer_joins(:buys).distinct.includes(:buys)
 
     if params[:customer_id].present?
       @customer = Customer.find_by(id: params[:customer_id])
       @purchasedetails = @customer&.purchasedetails&.includes(:product, buy: :customer) || []
+      @purchasedetails = @purchasedetails.where(buy_id: buy_ids) if buy_ids.present?
 
     elsif params[:id].blank? && params[:name].blank? && params[:documento].blank?
-      @purchasedetails = Purchasedetail.includes(:product, buy: :customer).all
+      @purchasedetails = Purchasedetail.includes(:product, buy: :customer)
+      @purchasedetails = @purchasedetails.where(buy_id: buy_ids) if buy_ids.present?
 
     elsif @customers.size == 1
       @customer = @customers.first
       @purchasedetails = @customer.purchasedetails.includes(:product, buy: :customer)
+      @purchasedetails = @purchasedetails.where(buy_id: buy_ids) if buy_ids.present?
     end
 
     @filter_result_empty = @customers.blank?
   end
+
 
 end
