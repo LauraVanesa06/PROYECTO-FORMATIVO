8277d18c6879a29d92033b32fe46c87618908803
KeinerL <168228409+KeinerL@users.noreply.github.com>
2025-07-06 12:20:50 -0500

    Feature/201 (#220)
    * 1
    
    * 2
    
    * conexion exitosa
    
    * .
    
    * .
    
    * graficas done
    
    * done
    
    * .
    
    * .

---
+++ backend/app/controllers/api/v1/dashboard_controller.rb
@@ -0,0 +1,62 @@
+class Api::V1::DashboardController < ApplicationController
+    def ventas_por_dia
+        ventas = Purchasedetail
+        .joins(:buy)
+        .group("DATE(buys.fecha)")
+        .sum("preciounidad * cantidad")
+
+        dias = {
+            "Monday" => "Lunes",
+            "Tuesday" => "Martes",
+            "Wednesday" => "Miércoles",
+            "Thursday" => "Jueves",
+            "Friday" => "Viernes",
+            "Saturday" => "Sábado",
+            "Sunday" => "Domingo"
+        }
+
+        render json: {
+        labels: ventas.keys.map { |d| dias[Date.parse(d).strftime("%A")] || d },
+        valores: ventas.values
+        }
+    end 
+    def inventario_por_categoria
+        data = Product.joins(:category)
+                        .group("categories.nombre")
+                        .sum(:stock)
+
+        render json: {
+            labels: data.keys,
+            valores: data.values
+        }
+    end
+    def ventas_mensuales
+        ventas = Purchasedetail.joins(:buy)
+            .group("strftime('%m', buys.fecha)")
+            .sum('preciounidad * cantidad')
+
+        meses_es = {
+            "01" => "Enero", "02" => "Febrero", "03" => "Marzo",
+            "04" => "Abril", "05" => "Mayo", "06" => "Junio",
+            "07" => "Julio", "08" => "Agosto", "09" => "Septiembre",
+            "10" => "Octubre", "11" => "Noviembre", "12" => "Diciembre"
+        }
+
+        datos_ordenados = ventas.sort.to_h
+
+        render json: {
+            labels: datos_ordenados.keys.map { |num| meses_es[num] },
+            valores: datos_ordenados.values
+        }
+    end
+    def resumen
+        render json: {
+        productos_en_stock: Product.sum(:stock),
+        ventas_hoy: Purchasedetail.joins(:buy)
+                        .where('date(buys.fecha) = ?', Date.today)
+                        .sum('preciounidad * cantidad'),
+        proveedores_registrados: Supplier.count,
+        clientes_registrados: Customer.count
+        }
+    end
+end
--- backend/config/application.rb
+++ backend/config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Appsena
   class Application < Rails::Application
+    config.load_defaults 7.0
+    config.i18n.default_locale = :es
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
