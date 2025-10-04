941eb9e35e8cc5dce6ffd05cd01915f33c62ab61
ELBERTOPERTUZ <168228624+ELBERTOPERTUZ@users.noreply.github.com>
2025-10-04 02:03:34 -0500

    main

--- ferremateriales/lib/main.dart
+++ ferremateriales/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'features/productos/bloc/cart_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'firebase_options.dart';
+import 'features/productos/bloc/cart_bloc.dart';
 import 'features/productos/bloc/product_bloc.dart';
 import 'features/auth/bloc/auth_bloc.dart';
 import 'features/auth/bloc/auth_event.dart';
 import 'features/auth/bloc/auth_state.dart';
+import 'features/productos/cubit/theme_cubit.dart';
 import 'features/auth/views/login_view.dart';
 import 'features/productos/views/main_view.dart';
 
@@ -16,7 +18,6 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  // 👇 Ocultar errores visuales rojos (como overflow)
   ErrorWidget.builder = (FlutterErrorDetails details) => const SizedBox.shrink();
 
   runApp(const FerreteriaApp());
@@ -32,39 +33,40 @@ class FerreteriaApp extends StatelessWidget {
         BlocProvider(create: (_) => AuthBloc()..add(AuthStarted())),
         BlocProvider(create: (_) => CartBloc()),
         BlocProvider(create: (_) => ProductBloc()),
+        BlocProvider(create: (_) => ThemeCubit()),
       ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          scaffoldBackgroundColor: const Color.fromARGB(255, 255, 255, 255),
-          textTheme: const TextTheme(
-            bodyMedium: TextStyle(color: Colors.black),
-            bodySmall: TextStyle(color: Colors.black),
-            titleLarge: TextStyle(color: Colors.black),
-          ),
-          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-            backgroundColor: Color.fromARGB(255, 250, 247, 247),
-            selectedItemColor: Color.fromARGB(255, 2, 2, 2),
-            unselectedItemColor: Colors.grey,
-            showUnselectedLabels: true,
-            type: BottomNavigationBarType.fixed,
-          ),
-        ),
-        home: BlocBuilder<AuthBloc, AuthState>(
-          builder: (context, authState) {
-            switch (authState.status) {
-              case AuthStatus.success:
-                return const MainView(); // 👈 Aquí ya tienes acceso al carrito
-              case AuthStatus.failure:
-              case AuthStatus.loggedOut:
-                return LoginView();
-              default:
-                return const Scaffold(
-                  body: Center(child: CircularProgressIndicator()),
-                );
-            }
-          },
-        ),
+      child: BlocBuilder<ThemeCubit, ThemeState>(
+        builder: (context, themeState) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: themeState.isDarkMode ? ThemeData.dark() : ThemeData.light(),
+            locale: themeState.locale,
+            supportedLocales: const [
+              Locale('es'),
+              Locale('en'),
+            ],
+            localizationsDelegates: const [
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            home: BlocBuilder<AuthBloc, AuthState>(
+              builder: (context, authState) {
+                switch (authState.status) {
+                  case AuthStatus.success:
+                    return const MainView();
+                  case AuthStatus.failure:
+                  case AuthStatus.loggedOut:
+                    return LoginView();
+                  default:
+                    return const Scaffold(
+                      body: Center(child: CircularProgressIndicator()),
+                    );
+                }
+              },
+            ),
+          );
+        },
       ),
     );
   }
