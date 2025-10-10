481fce111d16c203a4097011976163dc2dfa2340
Maria Sanchez <168229992+MajoSanchezA@users.noreply.github.com>
2025-10-10 15:47:51 -0500

    oki (#322)

--- ferremateriales/lib/features/productos/model/product_model.dart
+++ ferremateriales/lib/features/productos/model/product_model.dart
@@ -1,34 +1,39 @@
 import 'package:equatable/equatable.dart';
 
-
 class ProductModel extends Equatable {
   final int? id;
   final String? nombre;
   final String? descripcion;
   final double? precio;
   final int? stock;
-  final List<String>? imagenUrl;
+  final String? imagenUrl;
 
   const ProductModel({
     this.id,
     this.nombre,
     this.descripcion,
-    this.precio, 
+    this.precio,
     this.stock,
     this.imagenUrl,
   });
-  
+
   factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
-      id: json['id'] ?? 0,
-      nombre: json['nombre'] ?? '',
-      descripcion: json['descripcion'] ?? '',
-      precio: double.tryParse(json['precio'].toString()) ?? 0.0,
-      stock: int.tryParse(json['stock'].toString()) ?? 0,
-      imagenUrl: json['imagen_url'] != null
-          ? List<String>.from(json['imagen_url'])
-          : [],
-    );
+    id: json['id'] ?? 0,
+    nombre: json['nombre'] ?? '',
+    descripcion: json['descripcion'] ?? '',
+    precio: double.tryParse(json['precio'].toString()) ?? 0.0,
+    stock: int.tryParse(json['stock'].toString()) ?? 0,
+    imagenUrl:
+        json['imagen_url'] ?? "",
+  );
 
   @override
-  List<Object?> get props => [id, nombre, descripcion, precio, stock, imagenUrl];
+  List<Object?> get props => [
+    id,
+    nombre,
+    descripcion,
+    precio,
+    stock,
+    imagenUrl,
+  ];
 }
