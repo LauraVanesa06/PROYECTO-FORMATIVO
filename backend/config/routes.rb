95bb7dcf7cfe100ab8164317d9e300acfc3c09e6
KylieAndrea <168228628+KylieAndrea@users.noreply.github.com>
2025-10-11 17:12:03 -0500

    Feature/238 (#315)
    * productos
    
    * productos relacionados
    
    * contactos y img categ
    
    * favoritos
    
    * pausa
    
    * img carrito
    
    * .
    
    * vista carrito lateral
    
    * fin

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   get 'carrito', to: 'home#carrito', as: :carrito
   get 'notificaciones', to: 'home#notificaciones', as: :notificaciones
   get 'contactos', to: 'home#contacto', as: :contactos
+  get '/home/producto_show', to: 'home#producto_show', as: 'producto_show'
 
   # soporte pagina principal
   post '/contacto/enviar', to: 'home#send_report', as: :send_report
