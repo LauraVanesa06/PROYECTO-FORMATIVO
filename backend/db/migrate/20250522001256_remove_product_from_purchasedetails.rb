7ca01ecf116ed06ee656b338f388d763d5013795
ELBERTOPERTUZ <168228624+ELBERTOPERTUZ@users.noreply.github.com>
2025-10-15 15:28:48 -0500

    bug

--- backend/db/migrate/20250522001256_remove_product_from_purchasedetails.rb
+++ backend/db/migrate/20250522001256_remove_product_from_purchasedetails.rb
@@ -1,6 +1,8 @@
 class RemoveProductFromPurchasedetails < ActiveRecord::Migration[8.0]
   def change
-    remove_reference :purchasedetails, :product
+    if column_exists?(:purchasedetails, :product_id)
+      remove_reference :purchasedetails, :product
+    end
     remove_reference :purchasedetails, :buy, foreign_key: true
   end
 end
