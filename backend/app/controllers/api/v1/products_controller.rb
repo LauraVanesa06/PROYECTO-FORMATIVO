192a84577b03a87a49871f83b243d966ecbaf58e
KeinerL <keinerlindarte2@gmail.com>
2025-10-15 01:41:01 -0500

    .

--- backend/app/controllers/api/v1/products_controller.rb
+++ backend/app/controllers/api/v1/products_controller.rb
@@ -2,9 +2,17 @@ class Api::V1::ProductsController < ApplicationController
   skip_before_action :authenticate_user!
   
   def index
-    products = Product.all
+    # Top 10 productos más comprados
+    products = Product
+      .left_joins(:purchasedetails)
+      .group('products.id')
+      .select('products.*, COALESCE(SUM(purchasedetails.cantidad), 0) AS total_comprados')
+      .order('total_comprados DESC')
+      .limit(8)
+
     render json: products.map { |product|
       product.as_json.merge(
+        total_comprados: product.total_comprados.to_i,
         imagen_url: product.images.attached? ? url_for(product.images.first) : "NO_IMAGE",
         has_images: product.images.attached?,
         images_count: product.images.count
