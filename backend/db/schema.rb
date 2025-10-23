e5a1ec605b6b9631744bc5a5b80a0bad2e76e5b6
LauraVanesa06 <168228452+LauraVanesa06@users.noreply.github.com>
2025-10-23 14:41:52 -0500

    ...

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_13_023426) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_13_222019) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -49,7 +49,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_13_023426) do
     t.datetime "updated_at", null: false
     t.string "tipo"
     t.string "metodo_pago"
+    t.bigint "payment_id"
     t.index ["customer_id"], name: "index_buys_on_customer_id"
+    t.index ["payment_id"], name: "index_buys_on_payment_id"
   end
 
   create_table "cart_items", force: :cascade do |t|
@@ -214,6 +216,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_13_023426) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "buys", "customers"
+  add_foreign_key "buys", "payments"
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "products"
   add_foreign_key "carts", "users"
